f3ff35608592481afc2b0404fb8bd52d121ea0d9
Crislâine Santos <criis.santos@outlook.com>
2022-08-13 13:44:54 -0300

    🎉 setting up next.js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+
+import { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <h2>Ou</h2>
+  )
+}
+
+export default Home
